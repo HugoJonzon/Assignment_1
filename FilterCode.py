27e7e839036d6a7a5a54958c038ab86e78e01638
HugoJonzon <hugojonzon29@gmail.com>
2025-03-22 17:02:43 +0100

    Updated code. This version has no prints or user interface

--- FilterCode.py
+++ FilterCode.py
@@ -1,40 +1,35 @@
 import math
-class filter(): #Class for the filter 
+#This is a class that provides valuable data for a second-order high-pass or low-pass filter.
+#The class can be run by creating an object of the class and specifying "HP" for a high-pass filter or "LP" for a low-pass filter as the first attribute.
+#The class provides data such as the resonance point in hertz, the damping factor, and the transfer function.
+
+class Filter2: #Class for the filter 
+
     def __init__(self, type=None, Lvalue = None, Cvalue = None, Rvalue = 0): #Class for the filter 
         if type == "HP" or type == "LP":
             self.type = type
         else:
-            print("\n","Wrong input text","\n"), quit() #Prints an error if the input value is wrong and then quits
-
-        if Lvalue > 0 and Cvalue > 0 and Rvalue > 0:
-            self.Lvalue, self.Cvalue, self.Rvalue = Lvalue, Cvalue, Rvalue # Saves all the atributes is a objekt self
+            raise ValueError("Wrong input text.")
+        
+        if Lvalue > 0 and Cvalue > 0 and Rvalue >= 0:
+            self.Lvalue = Lvalue
+            self.Cvalue = Cvalue
+            self.Rvalue = Rvalue # Saves all the atributes is a objekt self
         else:
-            print("\n","Input values must be positive","\n"), quit() #Prints an error if the input value is wrong and then quits
+            raise ValueError("All component values must be positive.")
 
-    def getResonansPoint(self): #This function calculates the resonans point for the filter and then prints it 
+    def get_resonance_point(self): #This function calculates the resonans point for the filter and then prints it 
         resonansFreq = 1/(2*math.pi*math.sqrt(self.Lvalue*self.Cvalue)) 
-        print("\n","The resonans freaquency is:", round(resonansFreq, 4) ,"Hz")
         return round(resonansFreq, 4)
 
-    def getDampingFactor(self): # This function calculates the damping factor for the filter and then prints it 
+    def get_damping_factor(self): # This function calculates the damping factor for the filter and then prints it 
         dampingFactor = self.Rvalue/2*math.sqrt(self.Cvalue/self.Lvalue)
-        print("\n","The damping factor is:", round(dampingFactor, 4))
         return round(dampingFactor, 4)
-    
-    def getTransfer(self): # This gets the transfer function. The transfer function for high pass and low pass are differet 
+
+    def get_transfer(self): # This gets the transfer function. The transfer function for high pass and low pass are differet 
         if self.type == "LP":
-            transfer = "1 / (s^2*{} + s*{} + 1)".format(self.Lvalue * self.Cvalue, self.Rvalue * self.Cvalue) # Transfer funciton string
-            print("\n", "The transfer function for LP is:", transfer)
+            transfer = "1 / (s^2*{} + s*{} + 1)".format(self.Lvalue * self.Cvalue, self.Rvalue * self.Cvalue) # Transfer function string
         elif self.type == "HP":
             transfer = "(s^2*{}) / (s^2*{} + s*{} + 1)".format(self.Lvalue * self.Cvalue,self.Lvalue * self.Cvalue, self.Rvalue * self.Cvalue) 
-            print("\n", "The transfer function for HP is:", transfer)
         return transfer
-def main():
-    # This is all the input for the class
-    type = input("Choose wich type of filter. Write HP for high pass and LP for low pass: ")
-    Lvalue = float(input("Write an inductior value: "))
-    Cvalue = float(input("Write a capacitor value: "))
-    Rvalue = float(input("Write a resistor value: "))
-    myFilter = filter(type ,Lvalue, Cvalue, Rvalue) # Creats a object of the filter
-    myFilter.getResonansPoint(), myFilter.getDampingFactor(), myFilter.getTransfer() # Runs all the funcions in the class
-main()
+    
