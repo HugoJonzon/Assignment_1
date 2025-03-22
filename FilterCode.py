d44b5f801d9dd4ffaceef8c5271a5d84e39ed11c
HugoJonzon <hugojonzon29@gmail.com>
2025-03-22 17:10:40 +0100

    Reformated to pass ruff linter

--- FilterCode.py
+++ FilterCode.py
@@ -1,35 +1,40 @@
 import math
-#This is a class that provides valuable data for a second-order high-pass or low-pass filter.
-#The class can be run by creating an object of the class and specifying "HP" for a high-pass filter or "LP" for a low-pass filter as the first attribute.
-#The class provides data such as the resonance point in hertz, the damping factor, and the transfer function.
+# This is a class that provides valuable data for a second-order high-pass or low-pass filter.
+# The class can be run by creating an object of the class and specifying "HP" for a high-pass filter or "LP" for a low-pass filter as the first attribute.
+# The class provides data such as the resonance point in hertz, the damping factor, and the transfer function.
 
-class Filter2: #Class for the filter 
 
-    def __init__(self, type=None, Lvalue = None, Cvalue = None, Rvalue = 0): #Class for the filter 
+class Filter2:  # Class for the filter
+    def __init__(self, type=None, Lvalue=None, Cvalue=None, Rvalue=0):  # Class for the filter
         if type == "HP" or type == "LP":
             self.type = type
         else:
             raise ValueError("Wrong input text.")
-        
+
         if Lvalue > 0 and Cvalue > 0 and Rvalue >= 0:
             self.Lvalue = Lvalue
             self.Cvalue = Cvalue
-            self.Rvalue = Rvalue # Saves all the atributes is a objekt self
+            self.Rvalue = Rvalue  # Saves all the atributes is a objekt self
         else:
             raise ValueError("All component values must be positive.")
 
-    def get_resonance_point(self): #This function calculates the resonans point for the filter and then prints it 
-        resonansFreq = 1/(2*math.pi*math.sqrt(self.Lvalue*self.Cvalue)) 
+    def get_resonance_point(self):  # This function calculates the resonans point for the filter and then prints it
+        resonansFreq = 1 / (2 * math.pi * math.sqrt(self.Lvalue * self.Cvalue))
         return round(resonansFreq, 4)
 
-    def get_damping_factor(self): # This function calculates the damping factor for the filter and then prints it 
-        dampingFactor = self.Rvalue/2*math.sqrt(self.Cvalue/self.Lvalue)
+    def get_damping_factor(self):  # This function calculates the damping factor for the filter and then prints it
+        dampingFactor = self.Rvalue / 2 * math.sqrt(self.Cvalue / self.Lvalue)
         return round(dampingFactor, 4)
 
-    def get_transfer(self): # This gets the transfer function. The transfer function for high pass and low pass are differet 
+    def get_transfer(self):  # This gets the transfer function. The transfer function for high pass and low pass are differet
         if self.type == "LP":
-            transfer = "1 / (s^2*{} + s*{} + 1)".format(self.Lvalue * self.Cvalue, self.Rvalue * self.Cvalue) # Transfer function string
+            transfer = "1 / (s^2*{} + s*{} + 1)".format(
+                self.Lvalue * self.Cvalue, self.Rvalue * self.Cvalue
+            )  # Transfer function string
         elif self.type == "HP":
-            transfer = "(s^2*{}) / (s^2*{} + s*{} + 1)".format(self.Lvalue * self.Cvalue,self.Lvalue * self.Cvalue, self.Rvalue * self.Cvalue) 
+            transfer = "(s^2*{}) / (s^2*{} + s*{} + 1)".format(
+                self.Lvalue * self.Cvalue,
+                self.Lvalue * self.Cvalue,
+                self.Rvalue * self.Cvalue,
+            )
         return transfer
-    
--- test_FilterCode.py
+++ test_FilterCode.py
@@ -2,25 +2,28 @@ import math
 import unittest
 from FilterCode import Filter2
 
-class TestFilter(unittest.TestCase):
-    def test_get_resonance_point(self):
-        self.assertAlmostEqual(Filter2("HP",1/(3.1415**2),4,5).get_resonance_point(),0.25)
 
-    def test_get_damping_factor(self):
-        self.assertAlmostEqual(Filter2("HP",5,2,3).get_damping_factor(),0.9487)
-          
-    def test_get_transfer(self):
+class TestFilter(unittest.TestCase):
+    def test_get_resonance_point(self):  # Checks numerical value for the resonance point
+        self.assertAlmostEqual(
+            Filter2("HP", 1 / (3.1415**2), 4, 5).get_resonance_point(), 0.25
+        )
+
+    def test_get_damping_factor(self):  # Checks numerical value for the damping factor
+        self.assertAlmostEqual(Filter2("HP", 5, 2, 3).get_damping_factor(), 0.9487)
+
+    def test_get_transfer(self):  # Check the output string for the transfer function
         transferLP = "1 / (s^2*{} + s*{} + 1)".format(20, 12)
-        transferHP = "(s^2*{}) / (s^2*{} + s*{} + 1)".format(20,20, 12) 
-        self.assertEqual(Filter2("LP",5,4,3).get_transfer(),transferLP)
-        self.assertEqual(Filter2("HP",5,4,3).get_transfer(),transferHP)
-        
-    def test_wrong_input_type(self):
+        transferHP = "(s^2*{}) / (s^2*{} + s*{} + 1)".format(20, 20, 12)
+        self.assertEqual(Filter2("LP", 5, 4, 3).get_transfer(), transferLP)
+        self.assertEqual(Filter2("HP", 5, 4, 3).get_transfer(), transferHP)
+
+    def test_wrong_input_type(self):  # Checks if an error is raised for wrong input of "type"
         with self.assertRaises(ValueError):
-            Filter2("WRONG",1,1,1)
-        
-    def test_wrong_val(self):
+            Filter2("WRONG", 1, 1, 1)
+
+    def test_wrong_val(self):  # Checks if an error is raised for wrong input of value
         with self.assertRaises(ValueError):
-            Filter2("LP",-1,1,1)
-            Filter2("LP",1,-1,1)
-            Filter2("LP",1,1,-1)
+            Filter2("LP", -1, 1, 1)
+            Filter2("LP", 1, -1, 1)
+            Filter2("LP", 1, 1, -1)
